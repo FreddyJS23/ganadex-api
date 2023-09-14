7ac7986a9453530b7451f7b8cb3b3792470dc988
Freddy Solarte <freddyjsolarte@gmail.com>
2023-09-14 15:28:36 -0400

    fix: change nombre de tabla al eliminar

--- database/migrations/2023_09_12_022746_create_tipo_ganados_table.php
+++ database/migrations/2023_09_12_022746_create_tipo_ganados_table.php
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tipo_ganados');
+        Schema::dropIfExists('ganado_tipos');
     }
 };
