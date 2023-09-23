5df93755ceb3c767924324ce0f230d02fb912403
Freddy Solarte <freddyjsolarte@gmail.com>
2023-09-23 15:28:21 -0400

    add evento para servicio realizado

---
+++ app/Listeners/RevisionServicio.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Models\Evento;
+use App\Events\ServicioHecho;
+use DateInterval;
+use DateTime;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class RevisionServicio
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+    
+    /**
+     * Handle the event. 
+     */
+    public function handle(ServicioHecho $event): void
+    {
+        $eventoGanado=Evento::firstWhere('ganado_id',$event->servicio->ganado->id);
+        $fechaServicio=new DateTime($event->servicio->fecha);
+        $proxRevision=$fechaServicio->add(new DateInterval('P40D'))->format('Y-m-d');
+        $eventoGanado->prox_revision=$proxRevision;
+        $eventoGanado->save();
+
+        
+    }
+}
