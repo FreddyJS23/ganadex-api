2722324a405cab6b7eb6c21c0fd58f877248fe30
Freddy Solarte <freddyjsolarte@gmail.com>
2023-10-12 18:14:29 -0400

    add factory estado actual de ganado

---
+++ database/factories/EstadoFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Estado>
+ */
+class EstadoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'estado'=>fake()->randomElement(['sano','fallecido','sano-pendiente_revision','sano-pendiente_servicio']),
+            'fecha_defuncion'=>fake()->optional()->date(),
+            'causa_defuncion'=>fake()->optional()->sentence(), 
+        ];
+    }
+}
