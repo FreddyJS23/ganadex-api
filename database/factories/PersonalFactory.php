57b56ed1457835354b2e092592363edf9e4c1a9c
Freddy Solarte <freddyjsolarte@gmail.com>
2023-10-13 22:17:35 -0400

    add factory personal

---
+++ database/factories/PersonalFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Personal>
+ */
+class PersonalFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'ci' => fake()->unique()->randomNumber(8),
+            'nombre' => fake()->firstName(),
+            'apellido' => fake()->lastName(),
+            'fecha_nacimiento' => fake()->date(),
+            'cargo' => fake()->jobTitle(),
+            'sueldo' => fake()->numberBetween($int=1,$max=100),
+        ];
+    }
+}
