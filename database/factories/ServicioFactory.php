389f1cae06db83918a3e154fd54f4a2a02dcdda2
Freddy Solarte <freddyjsolarte@gmail.com>
2023-10-14 13:39:18 -0400

    add factory servicio

---
+++ database/factories/ServicioFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Servicio>
+ */
+class ServicioFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'observacion'=>fake()->word(),
+            'tipo'=>fake()->randomElement(['inseminacion','monta']),
+            'fecha'=>fake()->date()
+        ];
+    }
+}
