67909c26b4139686cc79415a6843f31d86722230
Freddy Solarte <freddyjsolarte@gmail.com>
2023-10-14 12:37:29 -0400

    add factory revision

---
+++ database/factories/RevisionFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Revision>
+ */
+class RevisionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'diagnostico' => fake()->word(),
+            'tratamiento' =>  fake()->word(),
+            'fecha'=>fake()->date()
+        ];
+    }
+}
