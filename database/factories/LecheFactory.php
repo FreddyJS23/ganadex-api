b4425f2a63b3b5f5d20033ec0d627e8519f92cf4
Freddy Solarte <freddyjsolarte@gmail.com>
2023-10-14 15:31:58 -0400

    add factory leche

---
+++ database/factories/LecheFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Leche>
+ */
+class LecheFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'peso_leche'=>fake()->randomNumber(4),
+            'fecha'=>fake()->date()
+        ];
+    }
+}
