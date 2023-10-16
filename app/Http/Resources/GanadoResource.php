b5f7269ca4fed02eeef0c3fe72b80f9e10841b81
Freddy Solarte <freddyjsolarte@gmail.com>
2023-10-16 16:47:18 -0400

    feat: add devolver infromacion eventos ganado

--- app/Http/Resources/GanadoResource.php
+++ app/Http/Resources/GanadoResource.php
@@ -15,20 +15,24 @@ class GanadoResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id'=>$this->id,
-            'nombre'=>$this->nombre,
-            'numero'=>$this->numero,
-            'origen'=>$this->origen,
-            'sexo'=>$this->sexo,
-            'tipo'=>$this->tipo->tipo,
-            'fecha_nacimiento'=>$this->fecha_nacimiento,
-            'peso_nacimiento'=>$this->peso->peso_nacimiento,
-            'peso_destete'=>$this->peso->peso_destete,
-            'peso_2year'=>$this->peso->peso_2year,
-            'peso_actual'=>$this->peso->peso_actual,
-            'estado'=>$this->estado->estado,
-            'fecha_defuncion'=>$this->estado->fecha_defuncion,
-            'causa_defuncion'=>$this->estado->causa_defuncion,
+            'id' => $this->id,
+            'nombre' => $this->nombre,
+            'numero' => $this->numero,
+            'origen' => $this->origen,
+            'sexo' => $this->sexo,
+            'tipo' => $this->tipo->tipo,
+            'fecha_nacimiento' => $this->fecha_nacimiento,
+            'peso_nacimiento' => $this->peso->peso_nacimiento,
+            'peso_destete' => $this->peso->peso_destete,
+            'peso_2year' => $this->peso->peso_2year,
+            'peso_actual' => $this->peso->peso_actual,
+            'estado' => $this->estado->estado,
+            'fecha_defuncion' => $this->estado->fecha_defuncion,
+            'causa_defuncion' => $this->estado->causa_defuncion,
+            'prox_revision' => $this->evento->prox_revision,
+            'prox_servicio' => $this->evento->prox_servicio,
+            'prox_parto' => $this->evento->prox_parto,
+            'prox_secado' => $this->evento->prox_secado,
         ];
     }
 }
