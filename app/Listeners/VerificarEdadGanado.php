51a3667ca10c1042b9f5ef32197b6563be983ed3
Freddy Solarte <freddyjsolarte@gmail.com>
2023-10-26 23:12:02 -0400

    fix: add comprobar si existe ganado
    En ocaciones cuando no hubiera ganado registrado, arrojaba un error de query que no podia devovler una collection vacia

--- app/Listeners/VerificarEdadGanado.php
+++ app/Listeners/VerificarEdadGanado.php
@@ -25,30 +25,31 @@ class VerificarEdadGanado
      */
     public function handle(Login $event): void
     {
-            $incializarFecha = new DateTime();
-            $fechaActual = $incializarFecha->format('Y-m-d');
-            //consulta sql, la diferencia seran los dias que le saca la fecha de nacimiento con la fecha actual
-            $sentenciaSqlDiferenciaDias = "DATEDIFF('$fechaActual',fecha_nacimiento) as diferencia";
-            $usuarioId=$event->user->getAuthIdentifier();
+        $incializarFecha = new DateTime();
+        $fechaActual = $incializarFecha->format('Y-m-d');
+        //consulta sql, la diferencia seran los dias que le saca la fecha de nacimiento con la fecha actual
+        $sentenciaSqlDiferenciaDias = "DATEDIFF('$fechaActual',fecha_nacimiento) as diferencia";
+        $usuarioId = $event->user->getAuthIdentifier();;
 
+        if (Ganado::where('user_id', $usuarioId)->count() > 0) {
             $becerros = Ganado::where('user_id', $usuarioId)
-                ->where('tipo_id',1)
+                ->where('tipo_id', 1)
                 ->select('tipo_id')
                 ->selectRaw($sentenciaSqlDiferenciaDias)
                 ->having('diferencia', '>=',  365)
                 ->having('diferencia', '<',  729)
                 ->get();
-                
-                $becerros->toQuery()->update(['tipo_id'=> 2]);
+
+            $becerros->toQuery()->update(['tipo_id' => 2]);
 
             $mautes = Ganado::where('user_id', $usuarioId)
-                ->where('tipo_id',2)
+                ->where('tipo_id', 2)
                 ->select('tipo_id')
                 ->selectRaw($sentenciaSqlDiferenciaDias)
-                ->having('diferencia', '>=',730)
+                ->having('diferencia', '>=', 730)
                 ->get();
-         
-                $mautes->toQuery()->update(['tipo_id'=> 3]);
-    
+
+            $mautes->toQuery()->update(['tipo_id' => 3]);
+        }
     }
 }
