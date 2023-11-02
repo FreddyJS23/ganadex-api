82407c82db03ec4b1882948b28c80154fc37711f
Freddy Solarte <freddyjsolarte@gmail.com>
2023-11-02 09:40:13 -0400

    refactor: add regeneracion de sesion

--- app/Http/Controllers/AuthLogin.php
+++ app/Http/Controllers/AuthLogin.php
@@ -15,19 +15,17 @@ class AuthLogin extends Controller
      */
     public function __invoke(LoginRequest $request)
     {
-           //buscar usuario
-           $user = User::firstWhere('usuario', $request->usuario);
+        //buscar usuario
+        $user = User::firstWhere('usuario', $request->usuario);
 
-           //usuario no encontrado
-           if (!$user) return response()->json(['message' => 'invalid user'], 401);
-             
-           
-           //intentar autenticar
-           if (Auth::attempt($request->only(['usuario','password']))) 
-            return response()->json(['login' => ['id' => $user->id,'usuario'=>$user->usuario,'token'=>$user->createToken('API_TOKEN')->plainTextToken]],200);  
-           
-           else  return response()->json(['message' => 'invalid password'], 401);
-           
-           
+        //usuario no encontrado
+        if (!$user) return response()->json(['message' => 'invalid user'], 401);
+
+
+        //intentar autenticar
+        if (Auth::attempt($request->only(['usuario', 'password']))) {
+            $request->session()->regenerate();
+            return response()->json(['login' => ['id' => $user->id, 'usuario' => $user->usuario, 'token' => $user->createToken('API_TOKEN')->plainTextToken]], 200);
+        } else  return response()->json(['message' => 'invalid password'], 401);
     }
 }
