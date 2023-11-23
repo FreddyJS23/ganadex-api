d0cbd6c6d8f961b0aac111841ec27ccd3f3439fe
Freddy Solarte <freddyjsolarte@gmail.com>
2023-11-23 09:46:09 -0400

    test: add dash board fallecimientos

---
+++ tests/Feature/DashboardFallecimientosTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Estado;
+use App\Models\Fallecimiento;
+use App\Models\Ganado;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class DashboardFallecimientosTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private int $cantidad_fallecimientos = 10;
+    private $estado;
+    private $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user
+            = User::factory()->create();
+
+        $this->estado = Estado::all();
+    }
+
+    private function generarFallecimiento(): Collection
+    {
+        return Fallecimiento::factory()
+            ->count($this->cantidad_fallecimientos)
+            ->for(Ganado::factory()->for($this->user)->hasAttached($this->estado))
+            ->sequence(['causa' => 'enferma'], ['causa' => 'accidente'])
+            ->create();
+    }
+
+
+    /**
+     * A basic feature test example.
+     */
+
+    public function test_causas_de_muertes_mas_frecuentes(): void
+    {
+        $this->generarFallecimiento();
+
+        $response = $this->actingAs($this->user)->getJson(route('dashboardFallecimientos.causasMuertesFrecuentes'));
+        $response->assertStatus(200)
+            ->assertJson(
+                fn (AssertableJson $json) =>  $json
+
+                    ->where('total_fallecidos', $this->cantidad_fallecimientos)
+                    ->whereType('causas_frecuentes', 'array')
+                    ->whereAllType(
+                        [
+                            'causas_frecuentes.0.fallecimientos' => 'integer',
+                            'causas_frecuentes.0.causa' => 'string'
+                        ]
+                    )
+
+            );
+    }
+}
