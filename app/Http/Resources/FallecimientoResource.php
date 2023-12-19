7bb4bbcd3fdb1ca2b20a0c98549f7e9626fd5613
Freddy Solarte <freddyjsolarte@gmail.com>
2023-12-19 17:20:07 -0400

    fix: add id espuesta json

--- app/Http/Resources/FallecimientoResource.php
+++ app/Http/Resources/FallecimientoResource.php
@@ -15,6 +15,7 @@ class FallecimientoResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id'=>$this->id,
             'fecha'=>$this->fecha,
             'causa'=>$this->causa,
             'numero_ganado'=>$this->ganado->numero,
