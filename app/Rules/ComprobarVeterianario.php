8083704a2d0091ddc66322f339854d137ab88cca
Freddy Solarte <freddyjsolarte@gmail.com>
2024-03-29 15:54:48 -0400

    create validation for check  veterinary
    consultara si el personal que se intentan registrar tiene el cargo de veterinario

---
+++ app/Rules/ComprobarVeterianario.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Rules;
+
+use App\Models\Personal;
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class ComprobarVeterianario implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        if (!Personal::where('id', $value)->where('cargo_id', 2)->first()) {
+            $fail('The :attribute must be veterinary');
+        }
+    }
+}
