5ebb545a8f1d5d8afba6e44f3ebe486dafe6814e
Freddy Solarte <freddyjsolarte@gmail.com>
2024-04-02 13:12:53 -0400

    refactor: keys response

--- app/Http/Resources/GanadoResource.php
+++ app/Http/Resources/GanadoResource.php
@@ -22,15 +22,9 @@ class GanadoResource extends JsonResource
             'sexo' => $this->sexo,
             'tipo' => $this->tipo->tipo,
             'fecha_nacimiento' => $this->fecha_nacimiento,
-            'peso_nacimiento' => $this->peso->peso_nacimiento,
-            'peso_destete' => $this->peso->peso_destete,
-            'peso_2year' => $this->peso->peso_2year,
-            'peso_actual' => $this->peso->peso_actual,
-            'estados' => $this->estados->makeHidden(['created_at','updated_at','pivot']),
-            'prox_revision' => $this->evento->prox_revision,
-            'prox_servicio' => $this->evento->prox_servicio,
-            'prox_parto' => $this->evento->prox_parto,
-            'prox_secado' => $this->evento->prox_secado,
+            'pesos'=>$this->peso,
+            'estados' => $this->estados,
+            'eventos'=>$this->evento,
         ];
     }
 }
