fb2b4205a90edc186ece68baf430f89922f3663c
Freddy Solarte <freddyjsolarte@gmail.com>
2024-04-04 14:28:48 -0400

    add routes for generate pdf

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ReportsPdfController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//rutas reportes pdf
+Route::get('/reportes/ganado/{ganado}', [ReportsPdfController::class, 'resumenGanado'])->name('reportes.ganado');
+Route::get('/reportes/general', [ReportsPdfController::class, 'resumenGeneral'])->name('reportes.general');
+Route::get('/reportes/venta_leche', [ReportsPdfController::class, 'resumenVentaLecheMesActual'])->name('reportes.ventaLeche');
+Route::get('/reportes/venta_ganado', [ReportsPdfController::class, 'resumenVentaGanadoAnual'])->name('reportes.ventaGanado');
+Route::get('/reportes/causas_fallecimientos', [ReportsPdfController::class, 'resumenCausasFAllecimientos'])->name('reportes.fallecimientos');
