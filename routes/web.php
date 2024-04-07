cf84c1ca7d6a67592ed9b1cf3fcb40efdbf1a565
Freddy Solarte <freddyjsolarte@gmail.com>
2024-04-07 09:11:58 -0400

    add routes to backup & restore BD

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BackupRestoreBDController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/respaldo',[BackupRestoreBDController::class,'respaldarBd']);
+Route::get('/restaurar', [BackupRestoreBDController::class, 'restaurarBd']);
