4fce5a2710e923ba5d795ae80164ed53859a5500
Freddy Solarte <freddyjsolarte@gmail.com>
2024-09-23 22:42:34 -0400

    fix bad data seeder vacuna

--- database/seeders/VacunaSeeder.php
+++ database/seeders/VacunaSeeder.php
@@ -16,40 +16,56 @@ class VacunaSeeder extends Seeder
         //tipo de animal 0, representa a que es aplicable a todo el rebaño
         //intervalo de dosis esta representado en días
 
-        DB::table('vacunas')->insert([
-            'nombre' => 'Fiebre aftosa',
-            'tipo_animal' => [0],
-            'intervalo_dosis' => 182,
+        /* se usa json_encode para poder almacenar un array de strings
+        como el modelo vacuna ya tiene un cast para el array no es necesario usar
+        el json_decode para obtener el array de strings */
+
+        DB::table("vacunas")->insert([
+            "nombre" => "Fiebre aftosa",
+            "tipo_animal" => json_encode(["rebano"]),
+            "intervalo_dosis" => 182,
         ]);
-        
-        DB::table('vacunas')->insert([
-            'nombre' => 'Costridial',
-            'tipo_animal' => [0],
-            'intervalo_dosis' => 180,
+
+        DB::table("vacunas")->insert([
+            "nombre" => "Costridial",
+            "tipo_animal" => json_encode(["rebano"]),
+            "intervalo_dosis" => 180,
         ]);
-        
-        DB::table('vacunas')->insert([
-            'nombre' => 'Rabia',
-            'tipo_animal' => [0],
-            'intervalo_dosis' => 365,
+
+        DB::table("vacunas")->insert([
+            "nombre" => "Rabia",
+            "tipo_animal" => json_encode(["rebano"]),
+            "intervalo_dosis" => 365,
         ]);
-        
-        DB::table('vacunas')->insert([
-            'nombre' => 'Leptospirosis',
-            'tipo_animal' => [4,3],
-            'intervalo_dosis' => 365,
+
+        DB::table("vacunas")->insert([
+            "nombre" => "Leptospirosis",
+            "tipo_animal" => json_encode(["novillo", "adulto"]),
+            "intervalo_dosis" => 365,
         ]);
-        
-        DB::table('vacunas')->insert([
-            'nombre' => 'IBR',
-            'tipo_animal' => [4,3],
-            'intervalo_dosis' => 365,
+
+        DB::table("vacunas")->insert([
+            "nombre" => "RB51",
+            "tipo_animal" => json_encode(["rebano"]),
+            "intervalo_dosis" => 20,
         ]);
-        
-        DB::table('vacunas')->insert([
-            'nombre' => 'Diarrea viral bobina',
-            'tipo_animal' => [4,3],
-            'intervalo_dosis' => 365,
+
+        DB::table("vacunas")->insert([
+            "nombre" => "RB51",
+            "tipo_animal" =>json_encode(["becerras"]),
+            "intervalo_dosis" => 150,
+        ]);
+
+        DB::table("vacunas")->insert([
+            "nombre" => "IBR",
+            "tipo_animal" => json_encode(["novillo", "adulto"]),
+            "intervalo_dosis" => 365,
+        ]);
+
+        DB::table("vacunas")->insert([
+            "nombre" => "Diarrea viral bobina",
+            "tipo_animal" => json_encode(["novillo", "adulto"]),
+            "intervalo_dosis" => 365,
         ]);
     }
 }
