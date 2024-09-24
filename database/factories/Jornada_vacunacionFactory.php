b95d14e46f09da1aecfb4019784adda23f316540
Freddy Solarte <freddyjsolarte@gmail.com>
2024-09-23 22:45:17 -0400

    add module jornda vacunacion

---
+++ database/factories/Jornada_vacunacionFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Vacuna;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Jornada_vacunacion>
+ */
+class Jornada_vacunacionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'fecha_inicio' => fake()->dateTimeThisYear()->format('y-m-d'),
+            'fecha_fin' => fake()->dateTimeThisYear()->format('y-m-d'),
+            'prox_dosis' => fake()->dateTimeThisYear()->format('y-m-d'),
+            'vacuna_id' => Vacuna::all()->random()->id,
+        ];
+    }
+}
---
+++ tests/Feature/JornadaVacunacionTest.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Jornada_vacunacion;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class JornadaVacunacionTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private array $jornadaVacunacion = [
+        'fecha_inicio' => '2020-10-02',
+        'fecha_fin' => '2020-10-02',
+        'vacuna_id' => 1,
+    ];
+
+    private int $cantidad_jornadasVacunacion = 10;
+    private $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user
+            = User::factory()->create();
+
+    }
+
+    private function generarJornadaVacunacion(): Collection
+    {
+        return Jornada_vacunacion::factory()
+            ->count($this->cantidad_jornadasVacunacion)
+            ->for($this->user)
+            ->create();
+    }
+    public static function ErrorInputProvider(): array
+    {
+        return [
+
+            'caso de insertar datos erróneos' => [
+                [
+                    'fecha_inicio' => '20201002',
+                    'fecha_fin' => '20201002',
+                    'vacuna_id' => 9393,
+                ],
+                ['fecha_inicio', 'fecha_fin', 'vacuna_id']
+            ],
+            'caso de no insertar datos requeridos' => [
+                [],
+                ['fecha_inicio', 'fecha_fin', 'vacuna_id']
+            ],
+        ];
+    }
+
+
+    public function test_obtener_jornadas_vacunacion(): void
+    {
+        $this->generarJornadaVacunacion();
+
+        $response = $this->actingAs($this->user)->getJson(route('jornada_vacunacion.index'));
+
+        $response->assertStatus(200)->assertJson(
+            fn(AssertableJson $json) =>
+            $json->whereType('jornadas_vacunacion', 'array')
+                ->has('jornadas_vacunacion', $this->cantidad_jornadasVacunacion)
+                ->has(
+                    'jornadas_vacunacion.0',
+                    fn(AssertableJson $json)
+                    => $json->whereAllType([
+                        'id' => 'integer',
+                        'fecha_inicio' => 'string',
+                        'fecha_fin' => 'string',
+                        'vacuna' => 'string',
+                    ])
+                )
+        );
+    }
+
+
+    public function test_creacion_jornada_vacunacion(): void
+    {
+
+        $response = $this->actingAs($this->user)->postJson(route('jornada_vacunacion.store'), $this->jornadaVacunacion);
+
+        $response->assertStatus(201)->assertJson(
+            fn(AssertableJson $json) => $json->whereAllType([
+                'jornada_vacunacion.id' => 'integer',
+                'jornada_vacunacion.fecha_inicio' => 'string',
+                'jornada_vacunacion.fecha_fin' => 'string',
+                'jornada_vacunacion.vacuna' => 'string',
+            ])
+        );
+    }
+
+    public function test_obtener_jornada_vacunacion(): void
+    {
+        $jornadasVacunacion = $this->generarJornadaVacunacion();
+        $idRandom = rand(0, $this->cantidad_jornadasVacunacion - 1);
+        $idJornadaVacunacion = $jornadasVacunacion[$idRandom]->id;
+
+        $response = $this->actingAs($this->user)->getJson(route('jornada_vacunacion.show', $idJornadaVacunacion));
+
+        $response->assertStatus(200)->assertJson(
+            fn(AssertableJson $json) => $json->whereAllType([
+                'jornada_vacunacion.id' => 'integer',
+                'jornada_vacunacion.fecha_inicio' => 'string',
+                'jornada_vacunacion.fecha_fin' => 'string',
+                'jornada_vacunacion.vacuna' => 'string',
+            ])
+        );
+    }
+
+    public function test_actualizar_jornada_vacunacion(): void
+    {
+        $jornadaVacunacion = $this->generarJornadaVacunacion();
+        $idRandom = rand(0, $this->cantidad_jornadasVacunacion - 1);
+        $idjornadaVacunacionEditar = $jornadaVacunacion[$idRandom]->id;
+
+        $response = $this->actingAs($this->user)->putJson(route('jornada_vacunacion.update', $idjornadaVacunacionEditar), $this->jornadaVacunacion);
+
+        $response->assertStatus(200)->assertJson(
+            fn(AssertableJson $json) =>
+            $json
+                ->where('jornada_vacunacion.fecha_inicio', $this->jornadaVacunacion['fecha_inicio'])
+                ->where('jornada_vacunacion.fecha_fin', $this->jornadaVacunacion['fecha_fin'])
+                ->whereAllType([
+                    'jornada_vacunacion.id' => 'integer',
+                    'jornada_vacunacion.fecha_inicio' => 'string',
+                    'jornada_vacunacion.fecha_fin' => 'string',
+                    'jornada_vacunacion.vacuna' => 'string',
+                ])
+                ->etc()
+        );
+    }
+
+
+    public function test_eliminar_jornada_vacunacion(): void
+    {
+        $jornadasVacunacion = $this->generarJornadaVacunacion();
+        $idRandom = rand(0, $this->cantidad_jornadasVacunacion - 1);
+        $idToDelete = $jornadasVacunacion[$idRandom]->id;
+
+        $response = $this->actingAs($this->user)->deleteJson(route('jornada_vacunacion.destroy', ['jornada_vacunacion'  => $idToDelete]));
+
+        $response->assertStatus(200)->assertJson(['jornada_vacunacionID' => $idToDelete]);
+    }
+
+    /**
+     * @dataProvider ErrorinputProvider
+     */
+    public function test_error_validacion_registro_jornadas_vacunacion($jornadaVacunacion, $errores): void
+    {
+        $response = $this->actingAs($this->user)->postJson(route('jornada_vacunacion.store'), $jornadaVacunacion);
+
+        $response->assertStatus(422)->assertInvalid($errores);
+    }
+}
