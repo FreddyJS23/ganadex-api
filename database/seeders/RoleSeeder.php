ed875f8b6cc767207e034a98bf71e8fb1b3e2126
Freddy Solarte <freddyjsolarte@gmail.com>
2024-11-28 17:40:27 -0400

    change seeders user roles

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class RoleSeeder extends Seeder
 {
@@ -13,6 +15,18 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('roles')->insert(['rol' => 'admin']);
+        $roleAdmin = Role::create(['name' => 'admin']);
+        $roleVeterinario = Role::create(['name' => 'veterinario']);
+
+        $permissionAdmin = Permission::create(['name' => 'todos']);
+
+        $permissionVerTodasFincas = Permission::create(['name' => 'observacion fincas']);
+    
+        $permissionVeterinario = Permission::create(['name' => 'limitado']);
+
+        $roleAdmin->givePermissionTo($permissionAdmin);
+
+        $roleVeterinario->givePermissionTo($permissionVeterinario);
+
     }
 }
