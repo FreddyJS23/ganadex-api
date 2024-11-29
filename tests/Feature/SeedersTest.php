99c622cb27eeb9c9b013a6ce5dd7190db8df8f37
Freddy Solarte <freddyjsolarte@gmail.com>
2024-11-29 01:42:46 -0400

    test: add seeder test

---
+++ tests/Feature/SeedersTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Database\Seeders\DemostracionSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class SeedersTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     */
+    public function test_seed_demostracion(): void
+    {
+        $this->seed(DemostracionSeeder::class);
+
+        $this->assertDatabaseHas('users', ['usuario' => 'admin']);
+    }
+}
