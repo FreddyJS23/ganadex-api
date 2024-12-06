3c08d3640ade3d4966ccab75e616329491aed966
Freddy Solarte <freddyjsolarte@gmail.com>
2024-12-05 23:17:09 -0400

    activate protection csrf token in all routes

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,8 +12,5 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-     /*    'api/login',
-        'api/register' */
-        '*'
     ];
 }
