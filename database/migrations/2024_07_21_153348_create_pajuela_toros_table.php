07cea119c29878f58cf94299daab28f6c2a6182c
Freddy Solarte <freddyjsolarte@gmail.com>
2025-01-19 10:33:15 -0400

    add columns

--- database/migrations/2024_07_21_153348_create_pajuela_toros_table.php
+++ database/migrations/2024_07_21_153348_create_pajuela_toros_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('pajuela_toros', function (Blueprint $table) {
             $table->id();
             $table->string('codigo')->unique();
+            $table->string('descripcion')->nullable($value=true);
+            $table->string('fecha');
             $table->foreignId('finca_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
