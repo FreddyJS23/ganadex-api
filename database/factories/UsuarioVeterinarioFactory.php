59c554f35b4d41d082d69cdd45219381d3bffe86
Freddy Solarte <freddyjsolarte@gmail.com>
2025-01-31 21:29:27 -0400

    add credentials to user in factory user veterinary

--- database/factories/UsuarioVeterinarioFactory.php
+++ database/factories/UsuarioVeterinarioFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Models\Personal;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\UsuarioVeterinario>
@@ -19,7 +20,9 @@ class UsuarioVeterinarioFactory extends Factory
     public function definition(): array
     {
         return [
-            'user_id' => User::factory()->create()->assignRole('veterinario'),
+            'user_id' => User::factory()
+            ->create(['usuario'=>'veterinario','password'=>Hash::make('123456')])
+            ->assignRole('veterinario'),
         ];
     }
 }
