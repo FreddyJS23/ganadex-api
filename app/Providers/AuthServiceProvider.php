94ecfae923fe14fd62cc3e417f9c3c1de1728073
Freddy Solarte <freddyjsolarte@gmail.com>
2025-02-08 01:32:34 -0400

    add authorization

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,8 +4,11 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 
+use App\Models\User;
+use App\Models\UsuarioVeterinario;
 use App\Policies\ConfiguracionPolicy;
 use App\Policies\FincaPolicy;
+use App\Policies\LogsVeterinarioPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -28,5 +31,9 @@ class AuthServiceProvider extends ServiceProvider
         Gate::define('verificar_sesion_finca',[FincaPolicy::class,'verificar_sesion_finca']);
         Gate::define('crear_sesion_finca',[FincaPolicy::class,'crear_sesion_finca']);
         Gate::define('update',[ConfiguracionPolicy::class,'update']);
-    }
+
+        Gate::define('view-logs', function (User $user, UsuarioVeterinario $usuarioVeterinario) {
+
+            return $user->hasRole('admin') &&  $user->id === $usuarioVeterinario->admin_id;
+        });    }
 }
