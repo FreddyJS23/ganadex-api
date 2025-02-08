a005adc2dad6f44cfa12a00fdb1e9ca02794b984
Freddy Solarte <freddyjsolarte@gmail.com>
2025-02-08 01:32:22 -0400

    create controller & resource

---
+++ app/Http/Controllers/ObtenerLogsVeterinario.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\LogsVeterinarioResource;
+use App\Models\UsuarioVeterinario;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use Spatie\Activitylog\Models\Activity;
+
+class ObtenerLogsVeterinario extends Controller
+{
+
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request, UsuarioVeterinario $usuarioVeterinario)
+    {
+
+        if (!Gate::allows('view-logs', $usuarioVeterinario)) {
+            abort(403);
+        }
+
+        $logsUsuarioVeterinario = Activity::where('causer_id', $usuarioVeterinario->user_id)->get();
+
+       return response()->json(['logs' => LogsVeterinarioResource::collection($logsUsuarioVeterinario)]);
+    }
+}
---
+++ app/Http/Resources/LogsVeterinarioResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class LogsVeterinarioResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $fecha = Carbon::create($this->created_at)->locale('es');
+        $fecha= "$fecha->dayName, " . $fecha->format('d-m-Y H:i:s');
+
+        return [
+            'id' =>$this->id,
+            'actividad'=>$this->subject_type ?  class_basename($this->subject_type) : $this->log_name,
+            'actividad_id'=>$this->subject_id,
+            'fecha'=>$fecha,
+        ];
+    }
+}
