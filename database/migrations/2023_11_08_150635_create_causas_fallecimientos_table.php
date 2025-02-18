c791f339cbafee3c5163c59c720203f7b5f710b5
Freddy Solarte <freddyjsolarte@gmail.com>
2025-02-17 22:21:02 -0400

    change colum table & add table

--- database/migrations/2023_11_08_150635_create_causas_fallecimientos_table.php
+++ database/migrations/2023_11_08_150635_create_causas_fallecimientos_table.php
@@ -11,12 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('fallecimientos', function (Blueprint $table) {
+        Schema::create('causas_fallecimientos', function (Blueprint $table) {
             $table->id();
-            $table->date('fecha');
-            $table->string('descripcion')->nullable();
-            $table->foreignId('causas_fallecimiento_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('ganado_id')->constrained()->cascadeOnDelete();
+            $table->string('causa');
             $table->timestamps();
         });
     }
@@ -26,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('fallecimientos');
+        Schema::dropIfExists('causas_fallecimientos');
     }
 };
