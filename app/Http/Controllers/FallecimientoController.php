677e8efc35736f04cd54259e245dc67b1ed37cf5
Freddy Solarte <freddyjsolarte@gmail.com>
2025-02-25 15:16:24 -0400

    Update FallecimientoController.php

--- app/Http/Controllers/FallecimientoController.php
+++ app/Http/Controllers/FallecimientoController.php
@@ -20,7 +20,7 @@ class FallecimientoController extends Controller
      */
     public function index()
     {
-        return new FallecimientoCollection(Fallecimiento::whereRelation('ganado', 'finca_id', [session('finca_id')][0])->with('ganado:id,numero')->get());
+        return new FallecimientoCollection(Fallecimiento::whereRelation('ganado', 'finca_id', session('finca_id'))->with('ganado:id,numero')->get());
     }
 
     /**
