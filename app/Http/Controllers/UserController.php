97ce72fccd9aa213a8df6bcc1f2ff7a8a69d91b1
Freddy Solarte <freddyjsolarte@gmail.com>
2025-04-08 23:54:07 -0400

    fix: error when register with docker
    como al momento de hacer la imagen el composer no instala dependencias de desarrollo, los factory no existen, por ende se crear manual

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -37,7 +37,23 @@ class UserController extends Controller
         $user->fill($request->except('password'));
         $user->assignRole('admin');
         $user->save();
-        Configuracion::factory()->for($user)->create();
+        $user->refresh();
+
+        //crear configuracion por defecto
+        $configuracion = new Configuracion();
+        $configuracion->fill([
+            /*Peso minimo para que este apta para un servicio  */
+            'peso_servicio' => 330,
+            /*Dias faltantes para crear una notificacion de evento proximo por ejemplo(evento proximo parto)*/
+            'dias_evento_notificacion' => 10,
+            /*Diferencia dias para que una vacuna se pueda posponer a una proxima jornada de vacunacion de la misma
+            vacuna, por ejemplo, si la dosis de vacuna individual es el 10-01 y hay una jornada el 20-01 entonces la
+            dosis de la vacuna individual se puede posponer hasta el 20-01 ya que la diferencia en dias es menor a 15
+            esto con el fin de llevar un control de vacunas jornada vacunacion a todo el rebaño*/
+            'dias_diferencia_vacuna' => 15,
+        ]);
+        $configuracion->user_id = $user->id;
+        $configuracion->save();
 
         return response()->json(['message' => 'usuario creado'], 201);
     }
