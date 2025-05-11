ad9021a09fec8550774397511c2d9b826953204b
Freddy Solarte <freddyjsolarte@gmail.com>
2025-05-11 01:51:58 -0400

    add cast date sale

--- app/Models/Venta.php
+++ app/Models/Venta.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Casts\Fecha;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -16,6 +17,9 @@ class Venta extends Model
         'ganado_id',
         'comprador_id',
         'hacienda_id'
+    ];
+    protected $casts = [
+        'fecha' => Fecha::class,
     ];
 
     /**
